81a126276d2daa9efdf30b4d60beb2437ffe5551
Grantoom <wolverin12@ya.ru>
2022-04-07 18:56:24 +0500

    Добавил notes, использовал базу данных SQL

--- Node/node.js
+++ Node/node.js
@@ -1,16 +1,24 @@
-const express = require ('express');
-
+const express = require('express');
 const app = express();
-
 const port = 3002;
 
-app.listen(port, () => {
-    console.log("My server")
+const {Pool, Client} = require('pg');
+const connectionString = 'postgressql://postgres:wolverin102@localhost:5432/porject1'
+
+app.listen(port,() => {
+    console.log('My server')
 })
 
-app.get('/',  (req, res, error) =>{
-    res.send('provet')
-}) 
-app.get('/kak-dela',  (req, res, error) =>{
-    res.send('provet')
+app.get('/notes' , (req, res, err) =>{
+    const client = new Client({
+        connectionString:connectionString
+    })
+    client.connect()
+    client.query('SELECT * FROM notes', (err, data)=>{
+        res.send(data.rows)
+        client.end()
+    })
+})
+app.get('/' , (req, res, err) =>{
+    res.send('Hello')
 })
