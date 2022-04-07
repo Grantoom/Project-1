275434c2d3c17843ef745e407b1435642ce109ed
Grantoom <wolverin12@ya.ru>
2022-04-07 20:20:50 +0500

    Добавил фильтр по параметрам запроса

--- Node/node.js
+++ Node/node.js
@@ -3,21 +3,34 @@ const app = express();
 const port = 3002;
 
 const {Pool, Client} = require('pg');
-const connectionString = 'postgressql://postgres:wolverin102@localhost:5432/porject1'
+const connectionString = 'postgressql://postgres:wolverin102@localhost:5432/project1'
 
 app.listen(port,() => {
     console.log('My server')
 })
 
 app.get('/notes' , (req, res, err) =>{
-    const client = new Client({
-        connectionString:connectionString
-    })
-    client.connect()
-    client.query('SELECT * FROM notes', (err, data)=>{
-        res.send(data.rows)
-        client.end()
-    })
+   // Если у нас есть текст в запросе, то фильтруем, а если нет текста в запросе, то выводим все заметки.
+    if (req.query.id) { 
+        const client = new Client({
+            connectionString:connectionString
+        })
+        client.connect()
+        client.query(`SELECT * FROM notes WHERE id = ${req.query.id}`, (err, data)=>{
+            res.send(data.rows)
+            client.end()
+        })
+    } else {
+        const client = new Client({
+            connectionString:connectionString
+        })
+        client.connect()
+        client.query('SELECT * FROM notes', (err, data)=>{
+            res.send(data.rows)
+            client.end()
+        })
+    }
+   
 })
 app.get('/' , (req, res, err) =>{
     res.send('Hello')
